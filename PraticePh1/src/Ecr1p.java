66090e8a629434ba271e4999fc6345be9018cc03
Tanmay <tripathitanmay18@gmail.com>
2020-12-18 10:09:44 +0530

    new commit for single file

---
+++ PraticePh1/src/Ecr1p.java
@@ -0,0 +1,10 @@
+
+public class Ecr1p {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiys Peeps i am a new file here,wyd??:)");
+
+	}
+
+}
