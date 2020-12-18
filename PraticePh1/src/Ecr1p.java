71516f7d0eeac1db610504bb84d2de1848f21d6e
Tanmay <tripathitanmay18@gmail.com>
2020-12-18 12:07:54 +0530

    Changed some shtuff add print line

--- PraticePh1/src/Ecr1p.java
+++ PraticePh1/src/Ecr1p.java
@@ -3,7 +3,7 @@ public class Ecr1p {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hiys Peeps i am a new file here,wyd??:)");
+		System.out.println("Hiya Peeps i am a new file here,wyd??:)");
 
 	}
 
---
+++ PraticePh1/src/jCw1/Ecr2p.java
@@ -0,0 +1,11 @@
+package jCw1;
+
+public class Ecr2p {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Howdy ho peeople whatcha doin");
+		System.out.println("howdy ho guys");
+	}
+
+}
